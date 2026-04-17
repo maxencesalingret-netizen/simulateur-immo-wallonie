80c0e489a55bfe913ede81d03db63e74d08b449e
maxencesalingret-netizen <maxence.salingret@gmail.com>
2026-04-17 08:40:54 +0200

    Delete simulateur-immo directory

--- simulateur-immo/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Change '/simulateur-immo-wallonie/' to match your GitHub repository name
-// e.g. if your repo is github.com/username/mon-simulateur, use '/mon-simulateur/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/simulateur-immo-wallonie/',
-})
